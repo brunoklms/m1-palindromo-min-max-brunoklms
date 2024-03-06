2d8b39a4419bd91de0f29e0bee99ff21eb7491d3
Bruno Lima <bruno.kauanlima1@hotmail.com>
2024-03-06 18:16:42 -0300

    Update script.js
    foi feito a lógica das duas funções

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    const strFormatada = str.replace(/\s/g, '').toLowerCase();
+
+    while(strFormatada === strFormatada.split('').reverse().join('')){
+        return true
+    }
+
+    return false
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let arrMin = arr[0];
+    let arrMax = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+       if(arr[i] < arrMin){
+        arrMin = arr[i];
+       } if(arrMax < arr[i]){
+        arrMax = arr[i];
+        }
+    }
+    return [arrMin, arrMax];
 }
